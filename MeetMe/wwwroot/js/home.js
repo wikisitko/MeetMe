039a76e3026d27d1b221b494ff89e784d7d56f76
Wiktoria Sitko <wsitko2000@gmail.com>
2021-11-26 00:25:55 +0100

    js

--- MeetMe/wwwroot/js/home.js
+++ MeetMe/wwwroot/js/home.js
@@ -1,1 +1,18 @@
-﻿
+﻿let welcomeOpened = false;
+
+function showWelcome() {
+    let element = document.getElementById("welcome");
+    element.style.display = welcomeOpened ? "none" : ""; //teraz element pojawi/schowa sie na ekranie
+    welcomeOpened = !welcomeOpened;
+}
+
+function sum() {
+    let inputs = document.getElementsByName("input"); //w rezultacie dostaniemy liste elementow html
+    let result = 0;
+    inputs.forEach(x => result += parseInt(x.value));
+    document.getElementById("result").innerHTML = result + "";
+
+    let span = document.createElement("span");
+    span.innerHTML = result + "";
+    document.getElementById("calculator").appendChild(span);
+}
